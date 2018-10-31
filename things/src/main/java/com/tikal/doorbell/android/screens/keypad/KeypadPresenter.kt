2e0b4bc717771b00b3fd0c3ce591d50e2a0b66f4
Moshe <pnemonic@gmail.com>
2018-10-31 17:02:23 +0200

    clean up code.

--- things/src/main/java/com/tikal/doorbell/android/screens/keypad/KeypadPresenter.kt
+++ things/src/main/java/com/tikal/doorbell/android/screens/keypad/KeypadPresenter.kt
@@ -1,8 +1,5 @@
 package com.tikal.doorbell.android.screens.keypad
 
-import android.util.Log
-import android.widget.Toast
-import com.tikal.doorbell.android.BaseView
 import com.tikal.doorbell.android.data.datasources.firebase.FirebaseRemoteDatesource
 import com.tikal.doorbell.android.data.repositories.firebase.FirebaseRepository
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -12,43 +9,36 @@ import timber.log.Timber
 
 class KeypadPresenter : KeypadContract.Presenter {
 
-    val repository: FirebaseRepository = FirebaseRepository(FirebaseRemoteDatesource())
-    lateinit var doorbellCode: String
-    lateinit var view: KeypadContract.View
-
-    companion object {
-        const val TAG = "KeypadPresenter"
-    }
+    private val repository: FirebaseRepository = FirebaseRepository(FirebaseRemoteDatesource())
+    private lateinit var doorbellCode: String
+    private lateinit var view: KeypadContract.View
 
     override fun subscribe(view: KeypadContract.View) {
-        Log.i(TAG, "KeypadPresenter: init")
+        Timber.i("KeypadPresenter: init")
         this.view = view
         subscribeDatabase()
     }
 
-    override fun unSubscribe() {
+    override fun unsubscribe() {
     }
-
 
     fun subscribeDatabase() {
         repository.getCode()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeBy(
-                        onNext = {
-                            doorbellCode = it
-                            Timber.i("### $it")
-                            view.toast(it)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onNext = {
+                    doorbellCode = it
+                    Timber.i("### $it")
+                    view.toast(it)
 
-                        },
-                        onError = { Timber.e(it) }
-                )
+                },
+                onError = { Timber.e(it) }
+            )
 
     }
 
     override fun onKeypadNumberClicked(num: Int) {
         Timber.i("### onKeypadNumberClicked $num")
     }
-
-
 }
