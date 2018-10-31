3bf30a78cb0af938c72a68b6889d5d19a0b42a56
Moshe <pnemonic@gmail.com>
2018-10-31 11:26:22 +0200

    LED for door indicator

---
+++ things/src/main/java/com/tikal/doorbell/android/DoorManager.kt
@@ -0,0 +1,48 @@
+package com.tikal.doorbell.android
+
+import com.google.android.things.pio.Gpio
+import com.google.android.things.pio.PeripheralManager
+import java.io.IOException
+
+/**
+ * Door manager.
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ *
+ * <pre>{@code
+ * val service = PeripheralManagerService()
+ * val mLedGpio = service.openGpio("BCM6")
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+ * mLedGpio.value = true
+ * }</pre>
+ * <p>
+ * @author moshe on 2018/10/31.
+ */
+class DoorManager {
+
+    companion object {
+        private const val LED_PIN_NAME = "BCM16"
+    }
+
+    private val service = PeripheralManager.getInstance()
+    private var ledGpio: Gpio? = null
+
+    init {
+        try {
+            val led = service.openGpio(LED_PIN_NAME)
+            led.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+            ledGpio = led
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun unlock() {
+        ledGpio?.value = true
+    }
+
+    fun lock() {
+        ledGpio?.value = false
+    }
+}
--- things/src/main/java/com/tikal/doorbell/android/MainActivity.kt
+++ things/src/main/java/com/tikal/doorbell/android/MainActivity.kt
@@ -2,8 +2,8 @@ package com.tikal.doorbell.android
 
 import android.app.Activity
 import android.os.Bundle
-import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 
 
 /**
@@ -30,20 +30,15 @@ class MainActivity : Activity() {
 
     private lateinit var database: FirebaseDatabase
     private lateinit var storage: FirebaseStorage
+    private lateinit var doorManager: DoorManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        database = FirebaseDatabase.getInstance();
-        storage = FirebaseStorage.getInstance();
-
-        // Initialize the doorbell button driver
-        initPIO()
-    }
-
-    private fun initPIO() {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        database = FirebaseDatabase.getInstance()
+        storage = FirebaseStorage.getInstance()
+        doorManager = DoorManager()
     }
 
     private fun handleAccessDenied() {
@@ -56,23 +51,19 @@ class MainActivity : Activity() {
         openDoor()
     }
 
-    /** Switch on the red LED. */
     private fun showAccessDenied() {
-        TODO("Switch on the red LED.")
     }
 
-    /** Switch on the green LED. */
     private fun showAccessGranted() {
-        TODO("Switch on the green LED")
     }
 
     /** Ensure the door is locked. */
     private fun lockDoor() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        doorManager.lock()
     }
 
-    /** Open the door. */
+    /** Unlock to open the door. */
     private fun openDoor() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        doorManager.unlock()
     }
 }
